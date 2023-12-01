e68c9480a83bfeabd33d5c899fad88d11924e802
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2023-12-01 10:01:19 -0400

    feat: add typeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ShunshineModule } from './shunshine/shunshine.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ShunshineModule],
+  imports: [
+    ShunshineModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'cinncinatus',
+      database: 'sunshine',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
