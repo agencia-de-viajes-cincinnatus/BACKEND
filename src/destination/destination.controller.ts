811212492ab52ebb040d239281df2b1413da2180
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2023-12-18 21:57:34 -0400

    feat: add s3 aws bucket to save image

--- src/destination/destination.controller.ts
+++ src/destination/destination.controller.ts
@@ -15,19 +15,7 @@ import { DestinationService } from './destination.service';
 import { UpdateDestinationDto } from './dtos/UpdateDestination.dto';
 import { CreateDestinationDto } from './dtos/CreateDestination.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { extname, join } from 'path';
-import { of } from 'rxjs';
-import * as fs from 'fs';
-
-const storage = {
-  storage: diskStorage({
-    destination: './uploads/destinationImages',
-    filename: (req, file, cb) => {
-      cb(null, `${req.params.id}${extname(file.originalname)}`);
-    },
-  }),
-};
+import { extname } from 'path';
 
 @Controller('api/destination')
 export class DestinationController {
@@ -43,12 +31,9 @@ export class DestinationController {
   }
 
   @Get('image/:imageName')
-  findImage(@Param('imageName') imageName: string, @Res() res) {
-    return of(
-      res.sendFile(
-        join(process.cwd(), 'uploads/destinationImages/' + imageName),
-      ),
-    );
+  async findImage(@Param('imageName') imageName: string, @Res() res) {
+    const url = await this.destinationService.getImage(imageName);
+    res.send({ url });
   }
 
   @Post()
@@ -57,19 +42,20 @@ export class DestinationController {
   }
 
   @Post('upload/:id')
-  @UseInterceptors(FileInterceptor('file', storage))
-  uploadFile(@Param('id', ParseUUIDPipe) id: string, @UploadedFile() file) {
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @Param('id', ParseUUIDPipe) id: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
     if (file.size !== 0) {
-      return this.destinationService.uploadFile(id, file.filename);
+      const fileName = `${id}${extname(file.originalname)}`;
+      return this.destinationService.uploadFile(
+        id,
+        fileName,
+        file.buffer,
+        file.mimetype,
+      );
     }
-
-    fs.unlink(`./uploads/destinationImages/${file.filename}`, (err) => {
-      if (err) {
-        console.error(err);
-      }
-      console.log('File deleted successfully');
-    });
-
     return {
       message: 'No file provided',
     };
